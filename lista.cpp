6d7dd447c4d8fd1d6687d82998c64d3c05375b7c
jeansousaiftm <61210073+jeansousaiftm@users.noreply.github.com>
2025-08-26 21:36:19 -0300

    Add files via upload

--- lista.cpp
+++ lista.cpp
@@ -20,10 +20,12 @@ struct List {
 
     Node *start;
     Node *end;
+    int c;
 
     List() {
         start = NULL;
         end = NULL;
+        c = 0;
     }
 
     bool empty() {
@@ -33,6 +35,7 @@ struct List {
     void pushBack(int value) {
 
         Node *n = new Node(value);
+        c++;
 
         if (empty()) {
             start = n;
@@ -45,11 +48,61 @@ struct List {
 
     }
 
+    void pushFront(int value) {
+
+        Node *n = new Node(value);
+        c++;
+
+        if (empty()) {
+            start = n;
+            end = n;
+            return;
+        }
+
+        n->next = start;
+        start = n;
+
+    }
+
+    void print() {
+        //printf("start: %d end: %d\n", start, end);
+        Node *aux = start;
+        while (aux != NULL) {
+            //printf("[%d] %d [%d] -> ", aux, aux->value, aux->next);
+            printf("%d -> ", aux->value);
+            aux = aux->next;
+        }
+    }
+
+    // complexidade tempo: O(n)
+    /*int size() {
+        int c = 0;
+        Node *aux = start;
+        while (aux != NULL) {
+            c++;
+            aux = aux->next;
+        }
+        return c;
+    }*/
+
+    // O(1)
+    int size() {
+        return c;
+    }
+
 };
 
 int main() {
 
+    List l;
 
+    l.pushBack(30);
+    l.pushBack(50);
+    l.pushBack(60);
+    l.pushFront(10);
+    l.pushFront(5);
+
+    l.print();
 
     return 0;
 }
