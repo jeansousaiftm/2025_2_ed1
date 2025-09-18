ff812265e33fb2d4d57dee4de611e001574b1428
jeansousaiftm <61210073+jeansousaiftm@users.noreply.github.com>
2025-09-18 20:10:40 -0300

    Add files via upload

--- lista.cpp
+++ lista.cpp
@@ -136,27 +136,163 @@ struct List {
 
     }
 
+    // O(x * n)
+    void atividade1(int x) {
+
+        for (int i = 0; i < x; i++) {
+            popBack();
+        }
+
+    }
+
+    // O(1)
+    void atividade2() {
+
+        if (size() <= 1) return;
+        if (size() == 2) {
+            popBack();
+            return;
+        }
+
+        Node* second = start->next;
+        start->next = second->next;
+        delete(second);
+        c--;
+
+    }
+
+    // O(1)
+    void atividade3() {
+        pushBack(size());
+    }
+
+    // O(n)
+    void atividade4(int n) {
+        for (int i = 1; i <= n; i++) {
+            pushBack(i);
+        }
+    }
+
+    //O(n)
+    void atividade5(int x) {
+
+        if (size() <= 1) return;
+
+        Node* aux = start;
+        while (aux->next != end) {
+            aux = aux->next;
+        }
+
+        Node* n = new Node(x);
+        n->next = end;
+        aux->next = n;
+        c++;
+
+    }
+
+    // O(pos)
+    void insert(int x, int pos) {
+
+        if (pos <= 0) {
+            pushFront(x);
+            return;
+        }
+
+        if (pos >= size()){
+            pushBack(x);
+            return;
+        }
+
+        Node* aux = start;
+        for (int i = 0; i < pos - 1; i++) {
+            aux = aux->next;
+        }
+
+        Node* n = new Node(x);
+        n->next = aux->next;
+        aux->next = n;
+        c++;
+
+    }
+
+    // O(n)
+    void atividade5_nova(int x) {
+
+        if (size() <= 1) return;
+        insert(x, size() - 1);
+
+    }
+
+    // O(pos)
+    void remove(int pos) {
+
+        if (pos <= 0) {
+            popFront();
+            return;
+        }
+
+        if (pos >= size() - 1) {
+            popBack();
+            return;
+        }
+
+        Node* aux = start;
+        for (int i = 0; i < pos - 1; i++) {
+            aux = aux->next;
+        }
+
+        Node* toDel = aux->next;
+        aux->next = toDel->next;
+        delete(toDel);
+        c--;
+
+    }
+
+    // O(1)
+    void atividade2_nova() {
+        if (size() <= 1) return;
+        remove(1);
+    }
+
 };
 
 int main() {
 
     List l;
 
-    l.pushBack(10);
-    l.pushBack(3);
+    l.pushFront(10);
+    l.pushFront(3);
     l.pushBack(5);
-    l.pushBack(8);
+    l.pushBack(3);
+    l.popFront();
+    l.pushFront(8);
+    l.pushBack(50);
+    l.popBack();
+    l.pushFront(78);
+    l.pushBack(1);
+    l.popBack();
+    l.pushBack(75);
+    l.pushFront(25);
+    l.popFront();
+    l.pushBack(60);
+    l.popBack();
+    l.pushFront(34);
+    l.pushBack(80);
+    l.pushBack(30);
+    l.popFront();
+    l.pushBack(23);
+    l.popFront();
+    l.popBack();
+    l.pushFront(5);
+    l.pushFront(66);
+    l.popFront();
 
-    l.print();
-    l.popBack();
-    l.print();
-    l.popBack();
-    l.print();
-    l.popBack();
-    l.print();
-    l.popBack();
-    l.print();
-    l.popBack();
+    l.insert(4, 4);
+    l.insert(100, 8);
+
+    l.remove(6);
+    l.remove(3);
+
     l.print();
 
     return 0;
